dff5787816c4c8735a66e572ceedd9c61447c40a
Gutehall <mathias@dm3.io>
2024-01-07 13:51:53 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -37,8 +37,7 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashi
 $UPDATE && $INSTALL terraform packer vault
 
 # ansible
-wget -O- "https://keyserver.ubuntu.com/pks/lookup?fingerprint=on&op=get&search=0x6125E2A8C77F2818FB7BD15B93C4A3FD7BB9C367" | sudo gpg --dearmour -o /usr/share/keyrings/ansible-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/ansible-archive-keyring.gpg] http://ppa.launchpad.net/ansible/ansible/ubuntu $UBUNTU_CODENAME main" | sudo tee /etc/apt/sources.list.d/ansible.list
+sudo add-apt-repository --yes --update ppa:ansible/ansible
 $UPDATE && $INSTALL ansible
 
 # docker
