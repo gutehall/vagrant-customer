304ce3f1294c981b4e31d26cedfebb4ba8c49e5b
gutehall <mathias@dm3.io>
2024-01-19 13:32:53 +0100

    Minor changes

--- install.sh
+++ install.sh
@@ -52,17 +52,17 @@ $UPDATE && $INSTALL ansible
 npm install -g aws-cdk
 
 # docker - Only use if customer has got license
-sudo install -m 0755 -d /etc/apt/keyrings &&
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg &&
-    sudo chmod a+r /etc/apt/keyrings/docker.gpg
+# sudo install -m 0755 -d /etc/apt/keyrings &&
+#     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg &&
+#     sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
-echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
-    sudo tee /etc/apt/sources.list.d/docker.list >/dev/null &&
-    $UPDATE && $INSTALL docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# echo \
+#     "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+#   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
+#     sudo tee /etc/apt/sources.list.d/docker.list >/dev/null &&
+#     $UPDATE && $INSTALL docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-sudo usermod -aG docker vagrant
+# sudo usermod -aG docker vagrant
 
 # podman & podman-compose
 $UPDATE && $INSTALL podman
